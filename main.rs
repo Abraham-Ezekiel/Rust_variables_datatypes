2db834623f4eb946994dde580a510922a7cd35c0
Abraham <akpanabraham001@gmail.com>
2024-11-23 12:20:06 +0100

    Rust variables and datatypes

---
+++ main.rs
@@ -0,0 +1,68 @@
+
+//variables and shadowing
+/*
+fn main() {
+	
+	println!("Hello, world!");
+	let mut x = 4;
+	x = 3;
+	println!("x is: {}",x);
+	{
+		let x = "hello";
+		println!("{}",x);
+		println!("{}",x);	
+	}
+	
+	println!("{}",x);
+	
+	let x = x+3;
+        println!("x is: {}",x);
+	const SECONDS_IN_MINUTES: u32 = 60;
+	println!("{}",SECONDS_IN_MINUTES)
+	
+  {
+	let x: u8 = 4;
+	let y = x;
+	println!("{}, {}", x, y);
+  }
+}
+*/
+/*
+data types
+primitive and compound
+primitive
+
+fn main(){
+	println!("hello world");
+	let x = 2; // or let x: i32 = 2 or let x: u32 = 2 for unsigned integer
+	let floating_point = 10.31; // let floating_point: f64 = 10.31; as default // or any other type like f32 
+ 	let mybool = true; // or let mybool: bool = true
+	let letter = 'a'; //or let letter: char = 'a' it could be'4' or '#'or (any single character inside a single quote)
+}
+*/
+//compound
+//tuple
+
+/*
+fn main(){
+	//let tup: (i32, char, bool) = (2, 'r', true);
+	//let tup: (i8, char, bool) = (2, 'r', true);
+	//let tup: (i16, char, bool) = (2, 'r', true); // they are all different types
+	let mut tup: (i32, char, bool) = (2, 'r', true);
+	//tup.0 = 4;
+	tup = (5, 'h', false);
+	println!("{}", tup.2); 
+
+//array
+  {
+	let mut arr = [1, 2, 3, 4, 5]; // or let mut arr: [i32; 5] = [1, 2, 3, 4, 5];
+	arr[0] = 5;
+	println!("{}", arr[0])
+  }
+  
+
+}
+*/
+
+
+
